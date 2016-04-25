73b4831040331eac80a9bf03ad77eceb39840730
Yanioconjota <janioisacura@gmail.com>
2016-04-24 21:08:55 -0300

    Gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+
+gulp.task('vet', function(){
+  log('Analizing source with JSHint and JSCS');
+  return gulp
+    .src([
+      './src/**/*.js',
+      './*.js'
+    ])
+    .pipe(jscs())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
+    .pipe(jshint.reporter('fail'));
+});
+////////////////
+function log(msg){
+  if (typeof(msg) === 'object') {
+    for (var item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        util.log(util.colors.cyan(msg[item]));
+      }
+    }
+  } else {
+    util.log(util.colors.cyan(msg));
+  }
+}
